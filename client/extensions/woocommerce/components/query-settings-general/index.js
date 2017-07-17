adcf504cdf95411152c19a8813c83c627d1cee64
Daniel Rey Lopez <daniel.rey.lopez@automattic.com>
2017-07-17 15:14:31 +0100

    Added error notice with retry functionallity to the <QuerySettingsGeneral> component.

--- client/extensions/woocommerce/components/query-settings-general/index.js
+++ client/extensions/woocommerce/components/query-settings-general/index.js
@@ -4,12 +4,15 @@
 import { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { localize } from 'i18n-calypso';
+import { uniqueId } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import { fetchSettingsGeneral } from 'woocommerce/state/sites/settings/general/actions';
-import { areSettingsGeneralLoaded } from 'woocommerce/state/sites/settings/general/selectors';
+import { areSettingsGeneralLoaded, areSettingsGeneralLoadError } from 'woocommerce/state/sites/settings/general/selectors';
+import { errorNotice, removeNotice } from 'state/notices/actions';
 
 class QuerySettingsGeneral extends Component {
 	fetch( siteId ) {
@@ -24,10 +27,22 @@ class QuerySettingsGeneral extends Component {
 		}
 	}
 
-	componentWillReceiveProps( { siteId, loaded } ) {
-		//site ID changed, fetch new zones
+	componentWillReceiveProps( { siteId, loaded, error, translate } ) {
 		if ( siteId !== this.props.siteId && ! loaded ) {
+			// site ID changed, fetch new zones
 			this.fetch( siteId );
+		} else if ( ! this.props.error && error ) {
+			// just received an error fetching, show the notice to retry
+			const noticeId = uniqueId();
+			this.props.actions.errorNotice( translate( 'Some of your site settings couldn\'t be retrieved.' ), {
+				id: noticeId,
+				showDismiss: false,
+				button: translate( 'Try again' ),
+				onClick: () => {
+					this.fetch( siteId );
+					this.props.actions.removeNotice( noticeId );
+				}
+			} );
 		}
 	}
 
@@ -43,11 +58,14 @@ QuerySettingsGeneral.propTypes = {
 export default connect(
 	( state ) => ( {
 		loaded: areSettingsGeneralLoaded( state ),
+		error: areSettingsGeneralLoadError( state ),
 	} ),
 	( dispatch ) => ( {
 		actions: bindActionCreators(
 			{
 				fetchSettingsGeneral,
+				errorNotice,
+				removeNotice,
 			}, dispatch
 		)
-	} ) )( QuerySettingsGeneral );
+	} ) )( localize( QuerySettingsGeneral ) );
